09c9678d23cdbb67972b7ffd45a12d07c8d83bf9
torinmo <torinmo.work@gmailcom>
2026-03-04 23:59:11 +0000

    health checks

--- storefront/src/app/not-found.tsx
+++ storefront/src/app/not-found.tsx
@@ -13,7 +13,7 @@ export default function NotFound() {
     <div className="flex flex-col gap-4 items-center justify-center min-h-[calc(100vh-64px)]">
       <h1 className="text-2xl-semi text-ui-fg-base">Page not found</h1>
       <p className="text-small-regular text-ui-fg-base">
-        The page you tried to access does not exist.
+        The page you tried to access does not exist!
       </p>
       <Link
         className="flex gap-x-1 items-center group"
